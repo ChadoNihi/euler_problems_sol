2a51e2b4bd74aed14e0b46732c207b05c01f063d
ElectroFish <chadoishere@gmail.com>
2015-05-24 20:41:22 +0300

    Pr22 is done

--- EuPr22.rb
+++ EuPr22.rb
@@ -3,7 +3,20 @@ def calc_score(flname)
 	0.downto(-1) {|i| sortedNames[i].tr!('"',"")}
 	sortedNames.sort!
 
+	letters = [*'A'..'Z']
+	vals = [*1..letters.length]
+	lettersVals = Hash[letters.zip(vals)]
 
+	score = 0
+	i = 1
+	sortedNames.each do |name|
+		sum = 0
+		name.each_char {|ch| sum += lettersVals[ch]}
+		score += i * sum
+		i+=1
+	end
+
+	score
 end
 
 puts calc_score("p022_names.txt")
